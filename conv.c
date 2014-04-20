77bb220b7dbe97b237a5a91f7ef3e55353bc6826
bob <patrick.g.langan@gmail.com>
2014-04-20 14:46:44 -0400

    Converts obj to binary, reads it into array

--- conv.c
+++ conv.c
@@ -2,59 +2,93 @@
 #include <stdlib.h>
 #include <string.h>
 
+struct vertex {
+	float x;
+	float y;
+	float z;
+};
+
+struct face {
+	int a;
+	int b;
+	int c;
+};
+
 int main (int argc, char **argv)
 {
 	FILE *file;
 	char line[255];
 	unsigned int i = 0;
-	unsigned int j = 0;
-	unsigned int vertexNumber = 0;
-	unsigned int faceNumber = 0;
-	float *vertexArray;
-	int *faceArray;
+	unsigned int vertNum = 0;
+	unsigned int faceNum = 0;
+	struct vertex *vertices;
+	struct face *faces;
+
 
 	file = fopen ("test.obj", "r");
 
+	//Getting number of vertices and faces
 	while (fgets (line, 255, file)) {
 		if (line[0] == 'v')
-			vertexNumber += 1;
+			vertNum += 1;
 		if (line[0] == 'f')
-			faceNumber += 1;
+			faceNum += 1;
 	}
 
-	vertexArray = malloc (vertexNumber * 3 * sizeof(float));
-	faceArray = malloc (faceNumber * 3 * sizeof(int));
+	vertices = malloc (vertNum * sizeof(vertices[0]));
+	faces = malloc (faceNum * sizeof(faces[0]));
 
 	fseek (file, 0, SEEK_SET);
 
-	while (fgets (line, 255, file)) {
-		if (line[0] == 'v' || line[0] == 'f') {
-			char *split;
 
+	//Reading the file into the vertex and face arrays
+	while (fgets (line, 255, file)) {
+		char *split;
+		if (line[0] == 'v') {
 			split = strtok (line, " ");
 			split = strtok (NULL, " ");
-
-			while (split) {
-				if (line[0] == 'v') {
-					vertexArray[i] = atof (split);
-					i++;
-				} else {
-					faceArray[j] = atoi (split);
-					j++;
-				}
-				split = strtok (NULL, " ");
-			}
+			vertices[i].x = atof (split);
+			split = strtok (NULL, " ");
+			vertices[i].y = atof (split);
+			split = strtok (NULL, " ");
+			vertices[i].z = atof (split);
+			i++;
+		} else if (line[0] == 'f') {
+			i = 0;
+			break;
 		}
 	}
+	do {
+		char *split;
+		if (line[0] == 'f') {
+			split = strtok (line, " ");
+			split = strtok (NULL, " ");
+			faces[i].a = atoi (split);
+			split = strtok (NULL, " ");
+			faces[i].b = atoi (split);
+			split = strtok (NULL, " ");
+			faces[i].c = atoi (split);
+			i++;
+		}
+	} while (fgets (line, 255, file));
 
 	fclose (file);
 
+
+	//Writing the vertex and face arrays to a binary file
 	file = fopen ("out.bin", "w");
-	fwrite (vertexArray, sizeof(float), vertexNumber * 3, file);
+
+	fwrite (&vertNum, sizeof(int), 1, file);
+	fwrite (vertices, sizeof(vertices[0]), vertNum, file);
+
+	fwrite (&faceNum, sizeof(int), 1, file);
+	fwrite (faces, sizeof(faces[0]), faceNum, file);
+
 	fclose (file);
 
-	free (vertexArray);
-	free (faceArray);
+
+	free (vertices);
+	free (faces);
 
 	return 0;
 }
--- loader.c
+++ loader.c
@@ -1,27 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct vertex {
+	float x;
+	float y;
+	float z;
+};
+
+struct face {
+	int a;
+	int b;
+	int c;
+};
+
 int main (int argc, char **argv)
 {
 	FILE *file;
-	unsigned int length;
-	float *array;
+	unsigned int vertexNum;
+	unsigned int faceNum;
+	struct vertex *vertexArray;
+	struct face *faceArray;
 	unsigned int i;
+
 
 	file = fopen ("out.bin", "r");
 
-	fseek (file, 0, SEEK_END);
-	length = ftell (file);
-	fseek (file, 0, SEEK_SET);
+	fread (&vertexNum, sizeof(int), 1, file);
+	vertexArray = malloc (vertexNum * sizeof(vertexArray[0]));
+	fread (vertexArray, sizeof(vertexArray[0]), vertexNum, file);
 
-	array = malloc (length);
-
-	fread (array, 1, length, file);
+	fread (&faceNum, sizeof(int), 1, file);
+	faceArray = malloc (faceNum * sizeof(faceArray[0]));
+	fread (faceArray, sizeof(faceArray[0]), faceNum, file);
 
 	fclose (file);
 
-	for (i = 0; i < length / sizeof(float); i++)
-		printf ("%f\n", array[i]);
+
+	for (i = 0; i < vertexNum; i++) {
+		printf ("%f, ", vertexArray[i].x);
+		printf ("%f, ", vertexArray[i].y);
+		printf ("%f\n", vertexArray[i].z);
+	}
+
+	for (i = 0; i < faceNum; i++) {
+		printf ("%d, ", faceArray[i].a);
+		printf ("%d, ", faceArray[i].b);
+		printf ("%d\n", faceArray[i].c);
+	}
 
 	return 0;
 }
