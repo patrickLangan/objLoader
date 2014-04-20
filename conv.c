c9f344f424c605814aa0363c210c8150a79d89eb
bob <patrick.g.langan@gmail.com>
2014-04-20 01:55:54 -0400

    Wrote basic obj loader

---
+++ conv.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main (int argc, char **argv)
+{
+	FILE *file;
+	char line[255];
+	unsigned int i = 0;
+	unsigned int j = 0;
+	unsigned int vertexNumber = 0;
+	unsigned int faceNumber = 0;
+	float *vertexArray;
+	int *faceArray;
+
+	file = fopen ("test.obj", "r");
+
+	while (fgets (line, 255, file)) {
+		if (line[0] == 'v')
+			vertexNumber += 1;
+		if (line[0] == 'f')
+			faceNumber += 1;
+	}
+
+	vertexArray = malloc (vertexNumber * 3 * sizeof(float));
+	faceArray = malloc (faceNumber * 3 * sizeof(int));
+
+	fseek (file, 0, SEEK_SET);
+
+	while (fgets (line, 255, file)) {
+		if (line[0] == 'v' || line[0] == 'f') {
+			char *split;
+
+			split = strtok (line, " ");
+			split = strtok (NULL, " ");
+
+			while (split) {
+				if (line[0] == 'v') {
+					vertexArray[i] = atof (split);
+					i++;
+				} else {
+					faceArray[j] = atoi (split);
+					j++;
+				}
+				split = strtok (NULL, " ");
+			}
+		}
+	}
+
+	fclose (file);
+
+	for (i = 0; i < vertexNumber * 3; i++)
+		printf ("%f\n", vertexArray[i]);
+
+	for (i = 0; i < faceNumber * 3; i++)
+		printf ("%d\n", faceArray[i]);
+
+	free (vertexArray);
+	free (faceArray);
+
+	return 0;
+}
